fcfe675c402e80b03fc0af2b851a81b7cf5e69c6
ramdhannassyirah <ramdhannassyirahh@gmail.com>
2024-12-05 11:48:08 +0700

    Setup Tailwind CSS & Struktur Folder Atomic Design

---
+++ components/atoms/ButtonPrimary.vue
@@ -0,0 +1,14 @@
+<template>
+  <button :class="className" :type="type">
+    <slot />
+  </button>
+</template>
+<script setup>
+
+
+const props = defineProps({
+    className: String,
+    type: String
+})
+</script>
+<style></style>
---
+++ components/molecules/FormInput.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <label :for="label">
+            <slot />
+        </label>
+        <input :type="type" :id="label">
+    </div>
+</template>
+<script setup>
+
+const props = defineProps({
+    label: String,
+    type: String
+})
+</script>
+<style lang="">
+    
+</style>
